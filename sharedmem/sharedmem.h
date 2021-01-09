2cb0becea19152dc28f3581814c5b2ef94760068
99003168 <75420644+99003168@users.noreply.github.com>
2021-01-09 17:32:46 +0530

    Add files via upload

---
+++ sharedmem/sharedmem.h
@@ -0,0 +1,13 @@
+#ifndef SHAREDMEM_H_INCLUDED
+#define SHAREDMEM_H_INCLUDED
+
+#include<unistd.h>
+#include<sys/mman.h>
+#include<sys/shm.h>
+#include<fcntl.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<semaphore.h>
+
+#endif // SHAREDMEM_H_INCLUDED
