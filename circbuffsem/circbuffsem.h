6ddc64327bc8b41bd115b7c303beb3f270acb5a6
99003168 <75420644+99003168@users.noreply.github.com>
2021-01-09 17:28:29 +0530

    Add files via upload

---
+++ circbuffsem/circbuffsem.h
@@ -0,0 +1,10 @@
+#ifndef CIRCBUFFSEM_H_INCLUDED
+#define CIRCBUFFSEM_H_INCLUDED
+
+#include <pthread.h>
+#include <semaphore.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+
+#endif // CIRCBUFFSEM_H_INCLUDED
