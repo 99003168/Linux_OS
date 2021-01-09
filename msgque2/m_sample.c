916e384f8ef449c55fc1327076ae14ed641fea47
99003168 <75420644+99003168@users.noreply.github.com>
2021-01-09 17:31:03 +0530

    Add files via upload

---
+++ msgque2/m_sample.c
@@ -0,0 +1,8 @@
+#include "msgque2.h"
+
+int main()
+{
+	int i=10;
+	printf("%d",i);
+	return 0;
+}
---
+++ msgque2/msgque2.h
@@ -0,0 +1,14 @@
+#ifndef MSGQUE2_H_INCLUDED
+#define MSGQUE2_H_INCLUDED
+
+#include<mqueue.h>
+#include<fcntl.h>
+#include<unistd.h>
+#include<sys/wait.h>
+#include<unistd.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+
+#endif // MSGQUE2_H_INCLUDED
