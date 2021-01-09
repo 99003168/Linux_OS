ab7bf86e0b2645a329205b5edbb82c5686146a42
99003168 <75420644+99003168@users.noreply.github.com>
2021-01-09 17:28:11 +0530

    Add files via upload

---
+++ circbuffmutex/s_cirmutex.h
@@ -0,0 +1,10 @@
+#ifndef S_CIRMUTEX_H_INCLUDED
+#define S_CIRMUTEX_H_INCLUDED
+
+#include <pthread.h>
+#include <semaphore.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+
+#endif // S_CIRMUTEX_H_INCLUDED
